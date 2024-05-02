5fbd3b6ecf15def9384a62c372a4ea91682ba862
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2024-05-02 22:00:05 +0200

    Update AddPostDTO.cs

--- Twitter-API/PostService/DTOs/AddPostDTO.cs
+++ Twitter-API/PostService/DTOs/AddPostDTO.cs
@@ -8,6 +8,7 @@ namespace PostService.DTOs
 {
     public class AddPostDTO
     {
+        public int postId {  get; set; }
         public string PostText {  get; set; }
         public int TimelineID { get; set; }
         public DateTime postDate { get; set; }
